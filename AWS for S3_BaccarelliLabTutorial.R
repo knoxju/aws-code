48de430dfbc3c02f47b546815d8a1cc4caec1b75
knoxju <knoxju@gmail.com>
2019-01-22 16:35:05 -0500

    Update AWS for S3_BaccarelliLabTutorial.R

--- AWS for S3_BaccarelliLabTutorial.R
+++ AWS for S3_BaccarelliLabTutorial.R
@@ -7,12 +7,12 @@ install.packages("aws.s3")
 library(aws.s3)
 
 # Set up your keys and your region here. N Virginia is US-East-1
-Sys.setenv("AWS_ACCESS_KEY_ID" = "AKIAJ7I3FOCKQHT45GEQ",
-           "AWS_SECRET_ACCESS_KEY" = "kkaqQyDC3onuT3vYtSjF91PqLiMN3UAzcauv3bqp",
+Sys.setenv("AWS_ACCESS_KEY_ID" = "your_key_id_here,
+           "AWS_SECRET_ACCESS_KEY" = "your_key_here",
            "AWS_DEFAULT_REGION" = "us-east-1")
 
 #See your buckets 
 bucketlist()
 
 #Get the bucket you want
-get_bucket("juliaknox")
+get_bucket("your_bucket_name_here")
